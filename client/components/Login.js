1602466dda54711c52c4c107c4a254d5b805cc92
kerri <75552647+kerriannercrawford@users.noreply.github.com>
2021-06-08 15:14:47 -0400

    Modal changes

--- client/components/Login.js
+++ client/components/Login.js
@@ -4,7 +4,7 @@ import { Button, Container } from '@chakra-ui/react';
 export default function Login(props) {
   return (
     <Container centerContent className='login-container'>
-      <Button colorScheme='blue' onClick={() => props.useLoginStatus(true)}>
+      <Button onClick={() => props.useLoginStatus(true)}>
         Log in with Google
       </Button>
     </Container>
