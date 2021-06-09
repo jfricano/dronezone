548210e96a9cc88175d2f2aa913363b1b93c9419
tylerkneidl <tskneidl@gmail.com>
2021-06-09 14:50:13 -0400

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /tmp/
-port=$(aws ssm get-parameters --region us-east-1 --names PORT --with-decryption --query Parameters[0].Value)
-
 npm install
 PORT=3000 PG_URI=$PG_URI node server/server.js
 
