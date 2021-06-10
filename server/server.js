9c00b2588775cdf9ad93ca73c5a4284a281907ab
Jason Fricano <jason.fricano@gmail.com>
2021-06-10 02:07:18 -0400

    add cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const cookieParser = require('cookie-parser');
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const {
