8d314d359ec1cfb29fb223a8f9d88cc132d5f261
Jason Fricano <jason.fricano@gmail.com>
2021-06-10 02:07:57 -0400

    change login button for testing purposes

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Button, Container } from '@chakra-ui/react';
+import React from "react";
+import { Button, Container } from "@chakra-ui/react";
 
 export default function Login(props) {
   return (
-    <Container centerContent className='login-container'>
-      <Button onClick={() => props.useLoginStatus(true)}>
+    <Container centerContent className="login-container">
+      {/* <Button onClick={() => props.useLoginStatus(true)}> */}
+      <Button onClick={() => fetch("/login", { mode: "no-cors" })}>
         Log in with Google
       </Button>
     </Container>
